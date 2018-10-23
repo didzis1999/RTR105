1f2d1b7502acfc47fd69478f23cf390699f26e89
user <rembolita9@inbox.lv>
2018-10-23 13:22:37 +0300

    20181023_13_22

---
+++ test_strings.py
@@ -0,0 +1,100 @@
+'''
+str1 = "Hello"
+str2 = 'there'
+bob = str1 + str2
+print(bob)
+Hellothere
+str3 = '123'
+str3 = str3 + 1
+x = int(str3) + 1
+print(x)
+
+ '''
+
+'''
+fruit='banana'
+letter=fruit[1]
+print(letter)
+
+x=3
+w=fruit[x-1]
+print(w)
+'''
+
+'''
+zot='abc'
+print(zot[5])
+'''
+
+'''
+fruit='banana'
+print(len(fruit))
+'''
+
+'''
+fruit='banana'
+index=0
+while index<len(fruit):
+    letter=fruit[index]
+    print(index,letter)
+    index=index+1
+'''
+
+'''
+name='Didzis'
+for letter in name:
+    print(letter)
+
+
+index=0
+while index<len(name):
+    letter=name[index]
+    print(letter)
+    index=index+1
+    '''
+
+'''
+word='arbuuzs'
+count=0
+for letter in word:
+    if letter =='u':
+        count=count+1
+print(count)        
+'''
+
+'''
+s='Monty Python'
+print(s[0:4])
+print(s[6:7])
+print(s[6:20])
+'''
+
+'''
+a='Hello'
+b=a+' ''There'
+print(b)
+'''
+
+'''
+fruit='banana'
+'n'in fruit
+'m'in fruit
+'nan' in fruit
+if 'a'in fruit:
+    print('Found it!')
+'''
+
+
+greet='Hello BOB'
+zap=greet.lower()
+lol=greet.upper()
+print(zap)
+print(lol)
+
+
+'''
+x = '이광춘'
+<type 'str'>
+x='이광춘'
+type(x)
+'''
